143d93ee0215dfcb15030ec9a11042af3fce67d0
Arjunapk <110887291+Arjunapk@users.noreply.github.com>
2023-08-07 18:33:29 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const todosRoute = require("./routes/todosRoute");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/v1/todos", todosRoute);
 
